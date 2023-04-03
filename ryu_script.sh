b90d8492241abe8f607cbee05760118f35e869ef
parksk99 <qkrtjdrb99@naver.com>
2023-04-03 23:36:29 +0900

    스크립트 변경

--- ryu_script.sh
+++ ryu_script.sh
@@ -11,5 +11,14 @@ rm puuids/GOLD*.json
 rm puuids/SI*.json
 rm puuids/BRON*.json
 rm puuids/IRON*.json
+rm summoners/CHAL*.json
+rm summoners/GRAND*.json
+rm summoners/MAS*.json
+# rm summoners/DIA*.json
+# rm summoners/PLA*.json
+rm summoners/GOLD*.json
+rm summoners/SI*.json
+rm summoners/BRON*.json
+rm summoners/IRON*.json
 python get_puuid.py
 python get_matchid.py
